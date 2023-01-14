993180c827edaa19e6572553359f708f31a12607
Mandy Sia <65438576+Mandy-13@users.noreply.github.com>
2023-01-15 06:45:42 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from main import MainPage
+from apps import basic, model1
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 400px;
+        margin-left: -400px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+app = MainPage()
+
+st.write("""# Laundry Shop Modeling """)
+st.write("""**Classification**: K-Nearest Neighbour (KNN), Random Forest""")
+st.write("""**Ensemble**: Stacking all classification models""")
+st.write("""**Clustering**: K-Means Clustering""")
+st.write("""**Regression**: Support Vector Regression, Linear Regression""")
+st.write("""**Deep Learning**: Neural Network""")
+
+
+
+# Add all your application here
+if st.button('Test Model'):
+    app.add_app("Prediction Models", model1.run)
+
+app.add_app("Exploratory Data Analysis (EDA)", basic.run)
+app.run()
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+
+class MainPage:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run(self):
+        def change_time(hms):    
+            #minutes = (hms.hour * 60) + hms.minute + (hms.second / 60)
+            minutes = int(hms.strftime("%H%M%S"))
+            return minutes
+
+        with st.sidebar:
+            #create dataframe
+            columns = ['Time(minute)','Temperature(celcius)','Windspeed(km/h)','Humidity(%)','Timespent(minute)','Age Range']
+            df = pd.DataFrame(columns = columns)
+
+
+            time = change_time(st.time_input('Reach Time', dt.time(8, 45), key = "1"))
+            windspeed = st.sidebar.slider('Define the windspeed in km/h', 2.49, 13.8, 6.2, key = "2")
+            temperature = st.sidebar.slider('Define the temperature in celcius', 23.7, 29.6, 25.5, key = "3")
+            humidity = st.sidebar.slider('Define the humidity in percentage', 64, 94, 70, key = "4")
+            timespent = st.sidebar.slider('How long the cutomer spent there (in minute)', 11, 60, 32, key = "5")
+            ageRange = st.sidebar.slider('How old is the cutomer (years)', 18, 60, 30, key = "6")
+
+            record = {'Time(minute)': time,'Temperature(celcius)': temperature,'Windspeed(km/h)': windspeed,'Humidity(%)': humidity,'Timespent(minute)': timespent,'Age Range': ageRange}
+            df = df.append(record, ignore_index=True)
+            df = df.rename(index={0: 'Your Input'})
+
+            df_for_display = df.copy().astype(int)
+            st.table(df_for_display)
+
+        app = st.selectbox(
+            'Navigation Bar',
+            self.apps,
+            format_func = lambda app: app['title'])
+
+        app['function'](df)
+
+
+
+
+
