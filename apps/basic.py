837f8d9e8f75ad8a49db9d651d2e9e56525b14c3
Mandy-13 <mdsia13@gmail.com>
2023-01-15 23:25:24 +0800

    update streamlit

--- apps/basic.py
+++ apps/basic.py
@@ -32,85 +32,117 @@ def run(x, y):
     # -----------------------------------------------------------------------------------------------
     st.markdown(f'<p style="color:Purple;font-size: 30px;font-weight: bold;">Exploratory Data Analysis (EDA)</p>', unsafe_allow_html=True)
 
-    st.markdown('##### **Question 1: What are the extra Data Points that I can include?**')
+    st.text("")
+    st.markdown('#### **Question 1: What are the extra Data Points that I can include?**')
     st.markdown('We extracted an external weather dataset from Open Meteo API. In the API, we used the URL builder to extract 4 variables that we decided that might be impactful, which are the rainfall(mm), temperature(celcius), humidity(%), and wind speed(ms/hour).')
 
+    # -----------------------------------------------------------------------------------------------
     st.text("")
-    st.markdown('##### **Question 2: How to visualise the content?**')
+    st.markdown('#### **Question 2: How to visualise the content? *AND* Question 6: Relationships between variables?**')
 
+    st.text("")
+    st.markdown(f'<p style="color:Blue;font-size: 24px;font-weight: bold;">Customer Segmentation</p>', unsafe_allow_html=True)
+
+    st.text("")
     st.markdown('**1. Bar Chart - Distribution of Age of Customers**')
     st.markdown('From the figure below, we can see that most customers who visit the laundry in the dataset are from 40 to 50  years old.  The second and third most frequent groups are 30 to 40 years old, and 50 to 60 years old. 60 years old and above is the least.')
     open_image('histogram_age.png')
 
+    st.text("")
     st.markdown('**2. Stacked Bar Chart - Distribution of Race and Gender of Customers**')
     st.markdown('In general, more males are going to the laundry compared to females except for the Indians and Malays.')
     open_image('stacked_race_gender.png') 
 
-    st.markdown('**3. Line Graph - Count of Customers by Month**')
-    st.markdown('This can be explained by the monsoon season in Malaysia of always raining around the end of the year and some maybe cleaning up for the new year. ')
-    open_image('line_graph_customer_by_month.png') 
-    
-    st.markdown('**4. Line Graph - Count of Customers by Time (Hour)**')
-    st.markdown('The highest peak hour is at 4am. The second highest peak is after 9pm to 11pm and the third highest peak is around 10a.m. The most deserted time is 6am.')
-    open_image('line_graph_customer_by_time.png') 
-
-    st.markdown('**5. Bar Chart - Relationship of bringing kids along and the kids category**')
+    st.text("")
+    st.markdown('**3. Bar Chart - Relationship of bringing kids along and the kids category**')
     st.markdown('This can be explained that a toddler needs high attention to take care of the parents as they just started to walk and it would be dangerous to leave them alone at home.')
     open_image('bar-chart-customer-with-kids.png') 
 
-    st.markdown('**6. Stacked Bar Chart - Relationship between wash item and basket size**')
+
+    # -----------------------------------------------------------------------------------------------
+    st.text("")
+    st.markdown(f'<p style="color:Blue;font-size: 24px;font-weight: bold;">Time Series Trend of the Laundry Shop</p>', unsafe_allow_html=True)
+
+    st.text("")
+    st.markdown('**1. Time Sries Plot - Month on Month Trend of Count of Customers and Revenue (RM)**')
+    st.markdown('This can be explained by the monsoon season in Malaysia of always raining around the end of the year and some maybe cleaning up for the new year. Therefore, the company may grab this opportunity to organise sales and promotions to attract more customers to visit the shop. ')
+    open_image('MoM-trent-customer-count-revenue.png') 
+
+    st.text("")
+    st.markdown('**2. Line Graph - Count of Customers by Time (Hour)**')
+    st.markdown('The highest peak hour is at 4am. The second highest peak is after 9pm to 11pm and the third highest peak is around 10a.m. The most deserted time is 6am.')
+    open_image('line_graph_customer_by_time.png') 
+
+    # -----------------------------------------------------------------------------------------------
+    st.text("")
+    st.markdown(f'<p style="color:Blue;font-size: 24px;font-weight: bold;">Relation Between Features</p>', unsafe_allow_html=True)
+
+    st.text("")
+    st.markdown('**1. Pairwise Correlation Heatmap to understand the relationship of the continuous variables in our dataset**')
+    st.markdown('A strong negative correlation of -0.75 between temperature (Celcius) and humidity(%), and also -0.64 between wind speed(km/h) and humidity. When the temperature and wind speed increases, humidity decreases as evaporation occurs faster. Furthermore, the correlation between wind speed and temperature is 0.56, a strong positive correlation.')
+    open_image('pairwise_correlation_heatmap.png')
+
+    st.text("")
+    st.markdown('**2. Stacked Bar Chart - Relationship between wash item and basket size**')
     st.markdown('The percentage of bringing big basket with clothes has high chance than small basket.')
     open_image('stacked-basket-size-wash-item.png') 
 
-    st.text("")
 
     # -----------------------------------------------------------------------------------------------
-    st.markdown('##### **Question 3: What data transformation to the dataset?**')
+    st.text("")
+    st.markdown('#### **Question 3: What data transformation to the dataset?**')
     st.markdown('- Removing duplication records, if there is any.')
     st.markdown('- Change features type. Example object to datetime.')
     st.markdown('- Data correction. The records for With_Kids and Kids_Category columns are wrong.')
     st.markdown('- Replace null values using KNN Clustering for imputation. ')
 
-
+    
     # -----------------------------------------------------------------------------------------------
-    st.markdown('##### **Question 4: Do I need to perform data imbalance treatment?**')
+    st.text("")
+    st.markdown('#### **Question 4: Do I need to perform data imbalance treatment?**')
     st.markdown('The **“BuyDrinkLabel”** is used in the classification models. It is an imbalanced class as the data of customers who bought drinks are far greater than customers who did not buy drinks.')
-    st.markdown('Solution: SMOTE Oversampling')
+    st.markdown('######**Solution: SMOTE Oversampling**')
 
+    st.text("")
     st.markdown("**Data Sampling of original BuyDrinkLabel**")
     open_image('bar-chart-bias.png')
     st.warning('0 - Did not buy Drink: 442')
     st.warning('1 - Buy Drink: 2358')
 
+    st.text("")
     st.markdown("**Data Sampling of BuyDrinkLabel after resampling**")
-    open_image('bar-chart-bias.png') 
+    open_image('bar-chart-smote-bias.png') 
     st.warning('0 - Did not buy Drink: 2358') 
     st.warning('1 - Buy Drink: 2358')
     
-
+    
     # -----------------------------------------------------------------------------------------------
-    st.markdown('##### **Question 5: How about outliers and missing values?**')
+    st.text("")
+    st.markdown('#### **Question 5: How about outliers and missing values?**')
     st.markdown('Null values are handled by using **KNN Clustering** for imputation for both numerical and categorical values.')
-    st.markdown('##### **Before KNN Clustering: Count NULL Value for each column using bar chart** ')
+
+    st.text("")
+    st.markdown('###### **Before KNN Clustering: Count NULL Value for each column using bar chart** ')
     open_image('ori-dataset-count-null.png')
-    st.markdown('##### **After KNN Clustering: Count NULL Value for each column using bar chart** ')
+
+    st.text("")
+    st.markdown('###### **After KNN Clustering: Count NULL Value for each column using bar chart** ')
     st.info(df0.isnull().sum())
 
-    # -----------------------------------------------------------------------------------------------
-    st.markdown('##### **Question 6: Relationships between variables?**')
-    st.markdown('**Pairwise Correlation Heatmap** to understand the relationship of the continuous variables in our dataset')
-    open_image('pairwise_correlation_heatmap.png')
-    st.markdown('A strong negative correlation of -0.75 between temperature (Celcius) and humidity(%), and also -0.64 between wind speed(km/h) and humidity. When the temperature and wind speed increases, humidity decreases as evaporation occurs faster. Furthermore, the correlation between wind speed and temperature is 0.56, a strong positive correlation.')
 
 
     # -----------------------------------------------------------------------------------------------
+    st.text("")
     st.markdown(f'<p style="color:Purple;font-size: 30px;font-weight: bold;">Association Rule Mining</p>', unsafe_allow_html=True)
     st.markdown('**What features of customer is likely to spend more than RM20 in the laundry (High ROI customer)**')
     st.markdown('#### Solution: Frequent Patterns')
+
+    st.text("")
     st.markdown('There are 3 ways to intepret the result.')
     st.markdown('- Support Metric for single item') 
     st.markdown('- Top 5 Association Rule') 
 
+    st.text("")
     st.markdown('##### **Choose your item to view the support metric** ')
     col1, col2, col3, col4 = st.columns([1,1,1,1])
     df = pd.read_csv('supportItemset1.csv').drop('Unnamed: 0', axis=1)
